e391a393f94fa91a93b02d3b3bc18d4a00afb707
LessiGG <ivan-lesnichenko@mail.ru>
2022-02-12 15:28:38 +0300

    Второй коммит потому что удаленные файлы сохранились, а новые не добавились

---
+++ Assets/PixelCrew/Components/ReloadLevelComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PixelCrew.Components
+{
+    public class ReloadLevelComponent : MonoBehaviour
+    {
+        public void Reload()
+        {
+            var currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
---
+++ Assets/PixelCrew/ValueComponent.cs
@@ -0,0 +1,22 @@
+﻿using PixelCrew;
+using UnityEngine;
+
+namespace PixelCrew
+{
+    public class ValueComponent : MonoBehaviour
+    {
+        [SerializeField] private int _value;
+        private Hero _hero;
+        private float _positionX;
+
+        private void Awake()
+        {
+            _hero = FindObjectOfType<Hero>();
+        }
+
+        public void AddCoins()
+        {
+            _hero.AddCoins(_value);
+        }
+    }
+}
