039f1d112350038dbeb56839c4fba5309871af18
LessiGG <ivan-lesnichenko@mail.ru>
2022-03-22 19:52:32 +0300

    AddCoinsComponent.cs почему-то не попал в коммит :/

---
+++ Assets/PixelCrew/AddCoinsComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace PixelCrew
+{
+    public class AddCoinsComponent : MonoBehaviour
+    {
+        [SerializeField] private int _value;
+        private Hero _hero;
+
+        private void Awake()
+        {
+            _hero = FindObjectOfType<Hero>();
+        }
+
+        public void AddCoins()
+        {
+            _hero.AddCoins(_value);
+        }
+    }
+}
